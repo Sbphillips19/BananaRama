b13247a4433d1f2201d32408567a91100f269180
Stephen Phillips <apeelingape@gmail.com>
2021-03-04 23:27:08 -0700

    make it rain simplified

--- src/RainingBananas/Particles.js
+++ src/RainingBananas/Particles.js
@@ -1,31 +1,20 @@
 import React from 'react'
 import ParticleType1 from './ParticleType1'
+import ParticleType2 from './ParticleType2'
 
 const generateWholeNumber = (min, max) => min + Math.floor(Math.random() * (max - min))
 
-class Particles extends React.PureComponent {
+export default class Particles extends React.PureComponent {
   render() {
-    let { count: n } = this.props
+    let { count: n } = this.props;
     const particles = [];
-    const types = [ParticleType1]
+    const types = [ParticleType2, ParticleType1, ParticleType1];
 
-    while (n && n--) {
-      const Particle = types[generateWholeNumber(0, 3)]
-      particles.push(<Particle key={n} />)
+    while (n--) {
+      const Particle = types[generateWholeNumber(0, 3)];
+      particles.push(<Particle key={n} />);
     }
 
-    return (
-      <div
-        className="particles"
-        style={{
-          position: 'fixed',
-          top: 0,
-        }}
-      >
-        {particles}
-      </div>
-    )
+    return <div className="particles">{particles}</div>;
   }
 }
-
-export default Particles
